37e5c461d4601cdfaaba9e1d5a7cce6995b9e45d
Felig <Feligrando@ya.ru>
2019-06-16 14:00:11 +0300

    With corrections

--- homework1/task1/js/common.js
+++ homework1/task1/js/common.js
@@ -6,17 +6,20 @@
 ● при наличии цифр в имени переводить буквы и цифры через символ к верхнему или нижнему регистру
 ● разместить всё это в js- файл, подключенный к созданной пустой html - странице
 */
-var name = prompt("Введите имя");	// ищем в этой строке
-var target = /\d+/g;	// цель поиска (цифры от 0 до 9)
-var m;	// условный счетчик
-m = target.exec(name); //поиск
+let name = prompt("Введите имя");	// We are looking for in this line.
+let target = /\d+/g;	// Search target (numbers 0 through 9).
+let counter;	// Conditional counter.
+counter = target.exec(name); // Search.
 
-if (m !== null) {
-	var i = 0, a = name[0].toLowerCase();
-    while(++i < name.length)  i % 2 ? a += name[i].toUpperCase(): a += name[i].toLowerCase();
-    alert(a);
-	}	//вывод имени пользователя с переменным регистром символов
-
-else {
-	alert(name.split("").reverse().join(""));
-	} //вывод имени пользователя задом наперёд
+if (counter !== null) {
+	let i = 0; 
+	let countSymbol = name[0].toLowerCase();
+	while(++i < name.length)  {
+		countSymbol += i % 2 ? name[i].toUpperCase(): 
+		name[i].toLowerCase();
+	}
+	alert(countSymbol);
+	}	// display the user name with a variable case of characters.
+	else {
+		alert(name.split("").reverse().join(""));
+	} // output user name back to front.
--- homework1/task2/js/common.js
+++ homework1/task2/js/common.js
@@ -6,5 +6,20 @@ button_2.onclick = removeAll;
 
 function removeAll(){
 	div_1.remove();
-	document.body.innerHTML = "<header><p>Header</p></header><main><article><h1>Что такое Lorem Ipsum?</h1>	<p>Lorem Ipsum - это текст, часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартным текстом на латинице с начала XVI века. В то время некий безымянный печатник создал большую коллекцию 	размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил без 	заметных изменений пять веков, но и перешагнул в электронный дизайн.</p>	</article>	<nav>	<p>Navigation</p>	</nav>	<aside>	<p>Sidebar</p></aside></main><footer><p>Footer</p></footer>";
+	document.body.innerHTML = "<header><p>Header</p></header>\
+	<main>\
+	<article>\
+	<h1>Что такое Lorem Ipsum?</h1>\
+	<p>Lorem Ipsum - это текст, часто используемый в печати и вэб-дизайне.\
+	 Lorem Ipsum является стандартным текстом на латинице с начала XVI века. \
+	 В то время некий безымянный печатник создал \
+	 большую коллекцию 	размеров и форм шрифтов, \
+	 используя Lorem Ipsum для распечатки образцов. \
+	 Lorem Ipsum не только успешно пережил без \
+	 заметных изменений пять веков, но и перешагнул в электронный дизайн.</p>\
+	 </article>\
+	 <nav><p>Navigation</p></nav>\
+	 <aside><p>Sidebar</p></aside>\
+	 </main>\
+	 <footer><p>Footer</p></footer>";
 }
